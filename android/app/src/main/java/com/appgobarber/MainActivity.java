d3afd08729b5ff2e13ee312bb9c95e7fd16851ff
sirmcclay <mcclay.design@gmail.com>
2020-09-29 12:55:39 -0300

    publish: Rebuild with com.appgobarbermmyapp name

--- android/app/src/main/java/com/appgobarber/MainActivity.java
+++ android/app/src/main/java/com/appgobarber/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.appgobarbermmyapp;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
--- android/app/src/main/java/com/appgobarber/SplashActivity.java
+++ android/app/src/main/java/com/appgobarber/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.appgobarbermmyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
