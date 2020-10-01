4bf89588d0cb496c05bb212a4d85c75ea5891fb0
sirmcclay <mcclay.design@gmail.com>
2020-09-30 21:48:31 -0300

    refactoy: .env production api address

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333', // Android Studio Emulator IP
+  // baseURL: 'http://10.0.2.2:3333', // Android Studio Emulator IP
+  baseURL: 'https://nodedeploy.mmyapp.com',
 });
 
 export default api;
